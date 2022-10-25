282b6fd77b92561940bdcdf7dc52e5c3a23551cb
Diego Rocha <diego.dpr@hotmail.com>
2022-10-25 11:10:52 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-bold text-2xl text-green-900">Hello World</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-green-900">
+        Enviar
+      </button>
+    </>
+  )
+}
