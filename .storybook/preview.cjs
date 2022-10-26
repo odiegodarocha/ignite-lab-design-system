ee4c012fedbd0822879354205a206a86674922e8
Diego Rocha <diego.dpr@hotmail.com>
2022-10-26 10:19:59 -0300

    pages signin, msw

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,13 @@
 import { themes } from '@storybook/theming'
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 import '../src/styles/global.css';
+
+initialize({
+  onUnhandledRequest: 'bypass'
+});
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,7 +17,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  dosc: {
+  docs: {
     theme: themes.dark,
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
+
+import { SignIn } from './pages/SignIn';
+
 import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className="text-bold text-2xl text-green-900">Hello World</h1>
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-green-900">
-        Enviar
-      </button>
-    </>
+    <SignIn />
   )
 }
